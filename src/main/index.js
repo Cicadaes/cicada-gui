93c6a462c3f09ceb3a4c8cebc5ada8e104b99dbc
Cicadaes <cicadaes@sina.com>
2018-03-23 23:38:06 -0700

    vue-devtools and debugger

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,13 @@
 import { app, BrowserWindow } from 'electron'
-// import electronDebug from 'electron-debug'
-// import installExtension from 'electron-devtools-installer'
+import electronDebug from 'electron-debug'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 
 let mainWindow
 
 // Install `electron-debug` with `devtron`
-// electronDebug({
-//   showDevTools: true
-// })
+electronDebug({
+  showDevTools: true
+})
 
 app.on('ready', () => {
   // Create main window
@@ -16,16 +16,17 @@ app.on('ready', () => {
     height: 450
   })
 
-  mainWindow.openDevTools()
+  // mainWindow.openDevTools()
 
-  // Install `vue-devtools`
-  // installExtension.default(installExtension.VUEJS_DEVTOOLS)
-  //   .then(() => {
-
-  //   })
-  //   .catch(err => {
-  //     console.log('Unable to intall `vue-devtools`: \n', err)
-  //   })
+  // Install `vue-devtools`, you should use vpn
+  // Error: spawn e:\7zip-lite\7z.exe ENOENT, just `node_modules\7zip\7zip-lite\7z.exe`
+  installExtension(VUEJS_DEVTOOLS)
+    .then(name => {
+      console.log(`Add extendsion: ${name}`)
+    })
+    .catch(err => {
+      console.log('Unable to intall `vue-devtools`: \n', err)
+    })
 
   mainWindow.loadURL('http://localhost:9001')
 
